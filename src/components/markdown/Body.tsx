4cdaf37255342280e8691614bfa1b41c0e731447
Pawel1894 <pawepohl@gmail.com>
2023-01-18 22:09:19 +0100

    Displaying active doc markup

--- src/components/markdown/Body.tsx
+++ src/components/markdown/Body.tsx
@@ -2,6 +2,14 @@ import React, { ReactElement } from "react";
 import { useAppSelector } from "../../hooks/redux";
 
 export default function Body(): ReactElement {
-  const content = useAppSelector((state) => state.docs.activeDoc?.content);
-  return <textarea>{content}</textarea>;
+  const activeDoc = useAppSelector((state) => state.docs.activeDoc);
+
+  if (!activeDoc) return <span>No document selected</span>;
+
+  return (
+    <textarea
+      className="w-screen min-h-screen p-4 font-mono text-sm"
+      value={activeDoc?.content ? activeDoc.content : ""}
+    />
+  );
 }
