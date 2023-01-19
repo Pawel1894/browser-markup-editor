0d159a73d4505b642ff96defc6c819433674be8e
Pawel1894 <pawepohl@gmail.com>
2023-01-19 22:16:16 +0100

    header bg color change

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,7 +7,7 @@ import Logo from "./Logo";
 
 export default function Header(): ReactElement {
   return (
-    <header className="bg-neutral-100 grid grid-cols-[56px_1fr] md:grid-cols-[72px_1fr] sticky top-0 ">
+    <header className="bg-neutral-300 grid grid-cols-[56px_1fr] md:grid-cols-[72px_1fr] sticky top-0 ">
       <AsideBtn />
       <div className="grid grid-cols-[1fr_18px_41px] md:grid-cols-[1fr_18px_152px] xl:grid-cols-[auto_1fr_18px_152px] w-full pl-6 pr-2 content-center gap-6">
         <Logo />
